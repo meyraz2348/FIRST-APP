1f64bcda8eb1f931e7f920c5ad960fc9d7052137
meyraz2348 <meyraz2348@gmail.com>
2023-02-21 03:05:09 +1100

    ui update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
 import { Paper, Container, Stack } from "@mui/material";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GuardComponent from "./Components/GuardComponent";
 import TemporaryDrawer from "./Components/TemporaryDrawer";
 import Sidings from "./Components/Sidings";
+import GuradWatch from "./Components/GuradWatch";
+import BasicTable from "./Components/DataTable";
 function App() {
   const [addGuards, setAddGuards] = useState(false);
   const [addSidings, setAddSidings] = useState(false);
-
+  const [guardWatch, setGuardWatch] = useState(false);
+  const [hasData, setHasData] = useState(false);
   const GuardHandler = () => {
     console.log("calling guards");
-
     setAddSidings(false);
+    setGuardWatch(false);
+
     setAddGuards(true);
     console.log("g", addGuards);
     console.log("s", addSidings);
   };
   const SidingHandler = () => {
     setAddGuards(false);
+    setGuardWatch(false);
+
     setAddSidings(true);
     console.log("adding sidings");
+  };
+  const GuardWatchHandler = () => {
+    setAddGuards(false);
+    setAddSidings(false);
+    setGuardWatch(true);
+    console.log("adding Guards");
+  };
+  const watchHandler = () => {
+    setHasData((prev) => !prev);
   };
 
   return (
     <>
-      <Stack spacing={2}>
-        <TemporaryDrawer onGuard={GuardHandler} onSiding={SidingHandler} />
+      <Stack spacing={2} direction="row">
+        <TemporaryDrawer
+          onGuard={GuardHandler}
+          onSiding={SidingHandler}
+          onGuardWatch={GuardWatchHandler}
+        />
         <Container
           style={{
             display: "flex",
@@ -35,10 +54,9 @@ function App() {
             marginTop: "6rem",
           }}
         >
-          <Paper style={{ width: "700px" }}>
-            {addGuards && <GuardComponent />}
-            {addSidings && <Sidings />}
-          </Paper>
+          {addGuards && <GuardComponent />}
+          {addSidings && <Sidings />}
+          {guardWatch && <GuradWatch onWatchHandler={watchHandler} />}
         </Container>
       </Stack>
     </>
